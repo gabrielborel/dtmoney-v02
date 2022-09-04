0141101790f76b29d30c300f6b28feff157417ab
Gabriel <biel_borel@hotmail.com>
2022-09-04 13:56:31 -0300

    Transactions context created

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+
+interface ITransaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  category: string;
+  value: number;
+  createdAt: string;
+}
+
+interface TransactionContextType {
+  transactions: ITransaction[];
+}
+
+const TransactionsContext = createContext({} as TransactionContextType);
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+export const TransactionsProvider = ({ children }: TransactionsProviderProps) => {
+  const [transactions, setTransactions] = useState<ITransaction[]>([]);
+
+  const loadTransactions = async () => {
+    const response = await fetch('http://localhost:3000/transactions');
+    const transactionsData = await response.json();
+    setTransactions(transactionsData);
+  };
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>{children}</TransactionsContext.Provider>
+  );
+};
+
+export const useTransactions = () => useContext(TransactionsContext);
